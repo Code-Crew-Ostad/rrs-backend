c57bec6f799c61e4da853a920d772391bf700c1b
AshiqueIftikhar <ashique.iftikhar@gmail.com>
2023-12-21 10:17:10 +0600

    port removed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app=require("./app");
-const PORT=5000;
-app.listen(PORT,function () {
-    console.log("App is running on port "+PORT);
+//const PORT=5000;
+app.listen(function () {
+    console.log("App is running on port");
 })
